582a635127ef1050d392482bb9d71ccca5b84ceb
MSDipper <github5248@gmail.com>
2022-11-13 18:31:55 +0300

    first commit

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8xt98^6kgy=*1!wiolxzci%x-4u7r+)3^-=$^!qz)fdjlbklux'
+SECRET_KEY = 'django-insecure-s=%um=hkh9rf-xbt#e+9w0vb^#!+j0%0o*jtq!wcx_5&p&i$^w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
