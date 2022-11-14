0069c4d9b32edfdbe40e6ba62144245e7f9a50a3
MSDipper <github5248@gmail.com>
2022-11-14 18:39:46 +0300

    Update admin.py

--- hotel/admin.py
+++ hotel/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
-# Register your models here.
+from hotel.models import (
+    Category,
+    ImageRoom, 
+    Hotel, 
+    Comment, 
+    RatingStar, 
+    Rating,
+    Reviews
+    )
