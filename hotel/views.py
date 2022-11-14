cc495a0ba49028aa1b668883dca677abf2c90350
MSDipper <github5248@gmail.com>
2022-11-14 22:23:14 +0300

    create functions output serializers list

--- hotel/views.py
+++ hotel/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from hotel.models import Hotel
+from hotel.serializers import HotelSerializer
 
-# Create your views here.
+
+class HotelListView(APIView):
+    ''' Вывод отелей '''
+    def get(self, request):
+        hotels = Hotel.objects.filter(published=True)
+        serializer = HotelSerializer(hotels, many=True)
+        return Response(serializer.data)
