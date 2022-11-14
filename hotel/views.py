b29c67264e46443a069c332daeffaaa5b6cdea0d
MSDipper <github5248@gmail.com>
2022-11-14 22:27:08 +0300

    added function for outpu apartments hotel

--- hotel/views.py
+++ hotel/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from hotel.models import Hotel
-from hotel.serializers import HotelSerializer
+from hotel.serializers import HotelSerializer, HotelDetailSerializer
 
 
 class HotelListView(APIView):
@@ -9,4 +9,12 @@ class HotelListView(APIView):
     def get(self, request):
         hotels = Hotel.objects.filter(published=True)
         serializer = HotelSerializer(hotels, many=True)
+        return Response(serializer.data)
+
+
+class HotelDetailView(APIView):
+    ''' Вывод апартаментов отеля''' 
+    def get(self, request, pk):
+        hotel = Hotel.objects.filter(id=pk, published=True)
+        serializer = HotelDetailSerializer(hotel, many=True)
         return Response(serializer.data)
