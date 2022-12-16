27a98a42094b8c7eb98e1025caf5a67eb59d78cd
MSDipper <github5248@gmail.com>
2022-12-16 20:56:54 +0300

    Update service.py

--- hotel/service.py
+++ hotel/service.py
@@ -1,4 +1,5 @@
-def get_client_ip(self, request):
+def get_client_ip(request):
+    """Получение IP пользоваеля"""
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
         ip = x_forwarded_for.split(',')[0]
