3a01e5bc250241f40ed717112d8fdb8a5b17b750
MSDipper <github5248@gmail.com>
2022-12-16 20:35:02 +0300

    urls for rating star

--- hotel/urls.py
+++ hotel/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from hotel.views import HotelListView, HotelDetailView, ReviewCreateView
+from hotel.views import (
+                        HotelListView,
+                        HotelDetailView, 
+                        ReviewCreateView,
+                        AddStarRatingView
+                        )
 
 
 urlpatterns = [
     path('hotel/<int:pk>/', HotelDetailView.as_view()),
     path('hotel/', HotelListView.as_view(), name='hotel'),
-    path('review/', ReviewCreateView.as_view())
+    path('review/', ReviewCreateView.as_view()),
+    path('rating/', AddStarRatingView.as_view()),
 ]
