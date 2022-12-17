8168e6be3d9473e0e84274841b7ca3f1e922df10
MSDipper <github5248@gmail.com>
2022-12-17 21:03:30 +0300

    Update views.py

--- hotel/views.py
+++ hotel/views.py
@@ -4,7 +4,7 @@ from rest_framework import generics
 from hotel.models import Hotel
 from hotel.service import get_client_ip, HotelFilter
 from django.db import models
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from hotel.serializers import (
                             HotelSerializer,
@@ -17,9 +17,9 @@ from hotel.serializers import (
 class HotelListView(generics.ListAPIView):
     ''' Вывод отелей '''
     serializer_class = HotelSerializer
-    filter_backends = (DjangoFilterBackend, SearchFilter)
+    filter_backends = (DjangoFilterBackend, SearchFilter, OrderFilter)
     filterset_class = HotelFilter
-    search_fields = ['title', 'quantity', 'gps']
+    search_fields = ['title', 'gps']
     
     
     def get_queryset(self):
